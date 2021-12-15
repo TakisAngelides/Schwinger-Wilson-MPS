084cc03fb02f1d5bb1aae477c6d3a620505cb0ec
TakisAngelides <takis.angelides@gmail.com>
2021-12-15 21:39:12 +0200

    First excited state algorithm now running smoothly

--- testing.jl
+++ testing.jl
@@ -1,10 +1,132 @@
-include("utility_functions.jl")
+using Profile
+using LinearAlgebra
+using Arpack
+using BenchmarkTools
+using Plots
+using LaTeXStrings
 using Test
+using HDF5
+include("MPO.jl")
+include("variational_first_excited_state_MPS_algorithm.jl")
 
-# https://github.com/kuehnste/mps.jl/blob/main/test/runtests.jl
+# ----------------------------------------------------------------------------------------------------------------------------------
 
-@testset "MPS Testing" begin
+# Checking that the spectrum of the exact Schwinger Hamiltonian agrees with the matrix formed by its MPO
 
-    @test 
+# N = 2
+# x = 1.0
+# m_g_ratio = 0.5
+# l_0 = 0.0
+# lambda = 0.0
 
-end
+# mpo = get_Schwinger_Wilson_MPO(N, l_0, x, lambda, m_g_ratio)
+
+# matrix = mpo_to_matrix(mpo)
+
+# matrix_h = get_Schwinger_hamiltonian_matrix(N, l_0, x, lambda, m_g_ratio)
+
+# display(norm(eigvals(matrix_h)-eigvals(matrix)))
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+
+# Checking that the minimum energy from the variational ground state search agrees with the minimum energy from exact diagonalization
+
+# N = 4
+# x = 1.0
+# m_g_ratio = 0.25
+# l_0 = 0.0
+# lambda = 0.0
+# acc = 10^(-10)
+# max_sweeps = 30
+# d = 2
+# D = 8
+# mpo = get_Schwinger_Wilson_MPO(N, l_0, x, lambda, m_g_ratio)
+# E_0, mps_ground, sn = variational_ground_state_MPS(2*N, d, D, mpo, acc, max_sweeps)
+# println("Minimum energy from variational ground state search: ", E_0)
+# matrix = mpo_to_matrix(mpo)
+# println("Minimum energy from exact diagonalization: ", minimum(eigvals(matrix)))
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+
+# Check that the penalty term enforces total charge to 0 and checking the local charge MPO
+
+# N = 4
+# x = 1.0
+# m_g_ratio = 0.5
+# l_0 = 0.25 # this is theta/2pi
+# lambda = 100.0
+# acc = 10^(-10)
+# max_sweeps = 10
+# d = 2
+# D = 10
+
+# penalty_mpo = get_penalty_term_MPO(N, lambda)
+# penalty_mpo_matrix = mpo_to_matrix(penalty_mpo)
+# penalty_matrix_exact = get_penalty_term_matrix(N, lambda)
+# display(eigvals(penalty_mpo_matrix))
+# display(eigvals(penalty_matrix_exact))
+
+# mpo = get_Schwinger_Wilson_MPO(N, l_0, x, lambda, m_g_ratio)
+# E_0, mps_ground, sn = variational_ground_state_MPS(2*N, d, D, mpo, acc, max_sweeps)
+
+# mpo_penalty = get_penalty_term_MPO(N, lambda)
+# mps_after_penalty_mpo = act_mpo_on_mps(mpo_penalty, mps_ground)
+
+# println(get_mpo_expectation_value(2*N, mps_ground, mpo_penalty))
+# println(get_spin_half_expectation_value(2*N, mps_ground, "z")) # the total charge operator is sum -g/2 sigma_z
+
+# charge_list = []
+
+# for n in 2:2:2*N
+
+#     charge_mpo = get_local_charge_MPO(N, n)
+#     mps_right = act_mpo_on_mps(charge_mpo, mps_ground)
+#     append!(charge_list, inner_product_MPS(mps_ground, mps_right))
+
+# end
+
+# display(charge_list)
+# println(sum(charge_list))
+
+# l_field = get_electric_field_configuration(N, l_0, mps_ground)
+
+# # This checks Gauss' law: l_field[i] - l_field[i-1] = Q[i]
+
+# display(isapprox(l_field[2]-l_field[1], charge_list[2]))
+# display(l_field[2]-l_field[1])
+# display(charge_list[2])
+
+# display(sum(get_electric_field_configuration(N, l_0, mps_ground))/N)
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+
+# Check that the first excited energy is the same as from exact diagonalization
+
+# N = 4
+# x = 1.0
+# m_g_ratio = 0.125
+# l_0 = 0.0
+# lambda = 100.0
+# acc = 10^(-10)
+# max_sweeps = 30
+# d = 2
+# D = 20
+# mpo = get_Schwinger_Wilson_MPO(N, l_0, x, lambda, m_g_ratio)
+# E_0, mps_ground, sn = variational_ground_state_MPS(2*N, d, D, mpo, acc, max_sweeps)
+# E_1, mps_first, sn_first = variational_first_excited_MPS(2*N, d, D, mpo, acc, max_sweeps, mps_ground, E_0)
+# println("First excited energy from variational search: ", E_1)
+# matrix = mpo_to_matrix(mpo)
+# println("First excited energy from exact diagonalization: ", eigvals(matrix))
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+
+# # https://github.com/kuehnste/mps.jl/blob/main/test/runtests.jl
+
+# @testset "MPS Testing" begin
+
+#     @test 
+
+# end
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+
--- variational_first_excited_state_MPS_algorithm.jl
+++ variational_first_excited_state_MPS_algorithm.jl
@@ -10,7 +10,7 @@ include("variational_MPS_algorithm.jl")
 
 function initialize_left_right_states(mps_first::Vector{Array{ComplexF64}}, mps_0::Vector{Array{ComplexF64}}, N::Int64)
 
-    p_states = Vector{Array{ComplexF64}}(undef, N+1) # This vector will hold the partial contractions of the left and right parts of the effective Hamiltonian see for example Schollwock Figure 38, 39
+    p_states = Vector{Array{ComplexF64}}(undef, N+1)
 
     p_states[1] = ones(ComplexF64, 1, 1)
     p_states[N+1] = ones(ComplexF64, 1, 1)
@@ -18,13 +18,12 @@ function initialize_left_right_states(mps_first::Vector{Array{ComplexF64}}, mps_
     for i in N:-1:2
 
         p_states[i] = contraction(conj(mps_first[i]), (3,), mps_0[i], (3,))
-        p_states[i] = contraction(p_states[i], (2,4), p_states[i+1], (1,2)) # Remember for loop index is going downwards so i+1 was the previous result in the for loop
+        p_states[i] = contraction(p_states[i], (2,4), p_states[i+1], (1,2))
 
     end
 
     return p_states
     
-
 end
 
 function update_p_states!(sweep_direction::Form, p_states::Vector{Array{ComplexF64}}, M::Array{ComplexF64}, mps_0_tensor::Array{ComplexF64}, i::Int64)
@@ -44,13 +43,13 @@ end
 
 function get_H_projection(left, mps_0_tensor, right)
 
-    tmp = contraction(left, (2,), mps_0_tensor, (1,))
-    B = contraction(tmp, (2,), right, (2,))
-    P = contraction(B, (), conj(B), ())
-    P = permutedims(P, (3,1,2,6,4,5))
+    tmp = contraction(left, (2,), mps_0_tensor, (1,)) 
+    B = contraction(tmp, (2,), right, (2,)) 
+    P = contraction(B, (), conj(B), ()) 
+    P = permutedims(P, (2,1,3,5,4,6)) 
     dimensions = size(P)
     P = reshape(P, (dimensions[1]*dimensions[2]*dimensions[3], dimensions[4]*dimensions[5]*dimensions[6]))
-
+    
     return P
 
 end
@@ -70,27 +69,21 @@ end
 
 function variational_first_excited_MPS(N::Int64, d::Int64, D::Int64, mpo::Vector{Array{ComplexF64}}, accuracy::Float64, max_sweeps::Int64, mps_0::Vector{Array{ComplexF64}}, E_0)
 
-    mps_first = initialize_MPS(N, d, D) # Initialize a random mps
-    gauge_mps!(right, mps_first, true, N) # Put it in right canonical form and normalize it
+    mps_first = initialize_MPS(N, d, D)
+    gauge_mps!(right, mps_first, true, N) 
 
-    # This are the partial contractions of the initial mps configuration which is contains all B tensors, 
-    # it has length N+1 where the first and last elements are 1x1x1 tensors of value 1. The states vector will thus be of the form
-    # 1RRRR1 for N = 5.
-
-    states = initialize_L_R_states(mps_first, mpo, N) # Holds partial contractions needed for each site's H_eff
+    states = initialize_L_R_states(mps_first, mpo, N) 
     p_states = initialize_left_right_states(mps_first, mps_0, N)
-    E_initial = 10^(-5) # Will hold the ground state energy approximation of the previous full sweep
-    E_optimal = 0 # Will hold the final best ground state energy approximation once the algorithm is finished
-    sweep_number = 0 # Counts the number of full sweeps performed
-    US = 0 # Will hold the residual matrices from SVD when we put a newly updated tensor in right canonical form while sweeping from right to left
+    E_initial = 10^(-5) 
+    E_optimal = 0 
+    sweep_number = 0 
+    US = 0 
 
     while(true)
         
-        E = 0 # Will hold the ground state energy apprxoimation right after a full sweep finishes
-
-        # From left to right sweep (right moving sweep or right sweep)
-
-        for i in 1:N-1 # Its up to N-1 here because the left moving sweep will start from N
+        E = 0 
+        
+        for i in 1:N-1 
 
             L = states[i]
             W = mpo[i]
@@ -99,12 +92,12 @@ function variational_first_excited_MPS(N::Int64, d::Int64, D::Int64, mpo::Vector
             p_right = p_states[i+1]
             M, _ = get_updated_site_first_excited(L, W, R, p_left, p_right, mps_0[i], E_0)
             mps_first[i], _ = gauge_site(left, M)
-            update_states!(right, states, mps_first[i], W, i+1) # i+1 because for loop starts from 1 and index 1 in states is the dummy 1x1x1 tensor of value 1 
+            update_states!(right, states, mps_first[i], W, i+1) 
             update_p_states!(right, p_states, mps_first[i], mps_0[i], i+1)
 
         end
 
-        for i in N:-1:2 # Lower limit is 2 here because the right moving sweep will start from 1
+        for i in N:-1:2 
 
             L = states[i]
             W = mpo[i]
@@ -112,7 +105,7 @@ function variational_first_excited_MPS(N::Int64, d::Int64, D::Int64, mpo::Vector
             p_left = p_states[i]
             p_right = p_states[i+1]
             M, E = get_updated_site_first_excited(L, W, R, p_left, p_right, mps_0[i], E_0)
-            US, mps_first[i] = gauge_site(right, M) # We only use US after this for loop to restore normalization to the mps state
+            US, mps_first[i] = gauge_site(right, M) 
             update_states!(left, states, mps_first[i], W, i)
             update_p_states!(left, p_states, mps_first[i], mps_0[i], i)
 
@@ -123,16 +116,6 @@ function variational_first_excited_MPS(N::Int64, d::Int64, D::Int64, mpo::Vector
         if fractional_energy_change < accuracy
 
             E_optimal = E
-            
-            # To restore the normalization of the mps
-
-            # We start with a normalized mps and each time we update a tensor we replace it with a normalized one such that the mps 
-            # normalization is maintained. The fact that we discard residual matrices from the SVD that put the updated tensors into 
-            # a particular gauge changes the mps and thus destroys its normalization. However these residual matrices would have been 
-            # multiplied on the next site which is about to get updated next and replaced with a normalized tensor, 
-            # restoring theoverall mps normalization. Just as we are about to stop sweeping, we need to multiply onto the tensor 
-            # on site 1 the lastresidual matrices from gauging site 2 so as to not change the mps, 
-            # maintaining in this way its normalization.
 
             mps_first[1] = contraction(mps_first[1], (2,), US, (1,))
             mps_first[1] = permutedims(mps_first[1], (1,3,2))
