c5760cbbca1fd30b4aaa2a522a2895d9b9e86040
TakisAngelides <takis.angelides@gmail.com>
2021-12-15 18:46:21 +0200

    Uncommented generate data function

--- run_Schwinger_Wilson.jl
+++ run_Schwinger_Wilson.jl
@@ -122,18 +122,20 @@ include("variational_first_excited_state_MPS_algorithm.jl")
 
 # Generate data for extrapolation to continuum - # N_list = [40, 60, 80, 100], D_list = [20, 40, 60, 80, 100]
 
+# Generated data for N = 20, D = 40
+
 # Generating data for N = 40, D = 20
 
-# accuracy = 10^(-10)
-# lambda = 100.0
-# l_0 = 0.0
-# max_sweep_number = 30
-# m_over_g_list = [0.125]
-# x_list = [30.0, 40.0, 50.0, 60.0, 80.0] # N/sqrt(x) > 5
+accuracy = 10^(-10)
+lambda = 100.0
+l_0 = 0.0
+max_sweep_number = 30
+m_over_g_list = [0.125]
+x_list = [30.0, 40.0, 50.0, 60.0, 80.0] # N/sqrt(x) > 5
 
-# N = parse(Int, ARGS[1])
-# D = parse(Int, ARGS[2])
+N = parse(Int, ARGS[1])
+D = parse(Int, ARGS[2])
 
-# generate_Schwinger_data(m_over_g_list, x_list, N, D, accuracy, lambda, l_0, max_sweep_number)
+generate_Schwinger_data(m_over_g_list, x_list, N, D, accuracy, lambda, l_0, max_sweep_number)
 
 # ----------------------------------------------------------------------------------------------------------------------------------
