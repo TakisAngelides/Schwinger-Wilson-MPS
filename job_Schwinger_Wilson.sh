8ba9fc074a1d5d53fdc60af569f90e10a9ae132c
TakisAngelides <takis.angelides@gmail.com>
2021-12-15 11:39:02 +0200

    First comit

---
+++ job_Schwinger_Wilson.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -o /onyx/qdata/TakisAngelides/logs/%x_%j.out
+#SBATCH -e /onyx/qdata/TakisAngelides/logs/%x_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --time=23:00:00
+#SBATCH --partition=phi
+#SBATCH --mem-per-cpu=8G
+
+/onyx/qdata/julia-1.7.0/bin/julia run_Schwinger_Wilson.jl 40 20
---
+++ testing.jl
@@ -0,0 +1,10 @@
+include("utility_functions.jl")
+using Test
+
+# https://github.com/kuehnste/mps.jl/blob/main/test/runtests.jl
+
+@testset "MPS Testing" begin
+
+    @test 
+
+end
