756abff87936db761da61a7e602343b38712b3fa
TakisAngelides <takis.angelides@gmail.com>
2021-12-17 16:37:18 +0200

    Parsing also x and mg now

--- run_Schwinger_Wilson.jl
+++ run_Schwinger_Wilson.jl
@@ -11,19 +11,19 @@ include("variational_first_excited_state_MPS_algorithm.jl")
 
 # ----------------------------------------------------------------------------------------------------------------------------------
 
-# Generate data for extrapolation to continuum - # N_list = [40, 60, 80, 100], D_list = [20, 40, 60, 80, 100]
+# Generate data for extrapolation to continuum - # N_list = [40, 60, 80, 100], D_list = [20, 40, 60, 80, 100], x_list = [30.0, 40.0, 50.0, 60.0, 80.0] # N/sqrt(x) > 5, mg = 0.125
 
 accuracy = 10^(-8)
 lambda = 100.0
 l_0 = 0.0
 max_sweep_number = 100
-m_over_g_list = [0.125]
-x_list = [30.0, 40.0, 50.0, 60.0, 80.0] # N/sqrt(x) > 5
 
 N = parse(Int, ARGS[1])
 D = parse(Int, ARGS[2])
 D_previous = parse(Int, ARGS[3])
+x = parse(Float, ARGS[4])
+mg = parse(Float, ARGS[5])
 
-generate_Schwinger_data(m_over_g_list, x_list, N, D, accuracy, lambda, l_0, max_sweep_number, D_previous)
+generate_Schwinger_data(mg, x, N, D, accuracy, lambda, l_0, max_sweep_number, D_previous)
 
 # ----------------------------------------------------------------------------------------------------------------------------------
