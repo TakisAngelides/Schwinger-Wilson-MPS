d54576694b73a142c329c4948276bb034471e9f2
TakisAngelides <takis.angelides@gmail.com>
2022-01-03 15:33:37 +0200

    Runs entropy mass plot function

--- job_Schwinger_Wilson.sh
+++ job_Schwinger_Wilson.sh
@@ -7,12 +7,13 @@
 #SBATCH --partition=phi
 #SBATCH --mem=31GB
 
-N=$1
-D=$2
-D_previous=$3
-x=$4
-mg=$5
+# N=$1
+# D=$2
+# D_previous=$3
+# x=$4
+# mg=$5
 
-#/onyx/qdata/julia-1.7.0/bin/julia run_Schwinger_Wilson.jl 100 100 80 80.0 0.125
-/onyx/qdata/julia-1.7.0/bin/julia run_Schwinger_Wilson.jl $1 $2 $3 $4 $5
-#/onyx/qdata/julia-1.7.0/bin/julia testing.jl
+# /onyx/qdata/julia-1.7.0/bin/julia run_Schwinger_Wilson.jl 100 100 80 80.0 0.125
+# /onyx/qdata/julia-1.7.0/bin/julia run_Schwinger_Wilson.jl $1 $2 $3 $4 $5
+# /onyx/qdata/julia-1.7.0/bin/julia testing.jl
+/onyx/qdata/julia-1.7.0/bin/julia run_Schwinger_Wilson.jl
