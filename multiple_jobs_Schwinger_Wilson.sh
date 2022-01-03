bf3c2f777a8050966e7be4eac34c152438cea614
TakisAngelides <takis.angelides@gmail.com>
2022-01-03 14:53:20 +0200

    Removed echo

--- multiple_jobs_Schwinger_Wilson.sh
+++ multiple_jobs_Schwinger_Wilson.sh
@@ -5,6 +5,10 @@ if [ -z $D ]; then
 echo "MISSING VALUE OF D"
 exit 1
 fi
+N_val=(40)
+x_val=(30.0 40.0 50.0 60.0 80.0)
+#N_val=(40)
+#x_val=(30.0)
 N_val=(40 60 80 100)
 x_val=(30.0 40.0 50.0 60.0 80.0)
 mg=0.125
@@ -17,6 +21,6 @@ fi
 
 for N in "${N_val[@]}"; do 
 	for x in "${x_val[@]}"; do 
-		echo sbatch -J TN_${N}_${D}_${D_previous}_${x}_${mg} job_Schwinger_Wilson.sh ${N} ${D} ${D_previous} ${x} ${mg}
+		sbatch -J TN_${N}_${D}_${D_previous}_${x}_${mg} job_Schwinger_Wilson.sh ${N} ${D} ${D_previous} ${x} ${mg}
 done
 done
