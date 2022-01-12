b8ec2b1759375965d859fee051364e133b7d57c1
TakisAngelides <takis.angelides@gmail.com>
2022-01-12 17:35:06 +0200

    p c

--- run_Schwinger_Wilson.jl
+++ run_Schwinger_Wilson.jl
@@ -29,7 +29,7 @@ include("variational_first_excited_state_MPS_algorithm.jl")
 
 # Generate data for entanglement entropy vs mass plot 
 
-D = 128
+D = 80
 accuracy = 10^(-8)
 lambda = 100.0
 l_0 = 0.0
