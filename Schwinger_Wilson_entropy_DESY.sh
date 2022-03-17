0947913e80b9af52e1fd7617c0810ac9c81d5890
TakisAngelides <takis.angelides@gmail.com>
2022-03-17 12:42:39 +0100

    removed debug echo

--- Schwinger_Wilson_entropy_DESY.sh
+++ Schwinger_Wilson_entropy_DESY.sh
@@ -7,6 +7,5 @@ D=$3
 
 mg_list=(-0.1 -0.2 -0.3 -0.45 -0.47 -0.49 -0.51 -0.53 -0.55 -0.6 -0.65 -0.75)
 for mg in "${mg_list[@]}" ;do
-    echo $mg
     /lustre/fs23/group/nic/tangelides/julia-1.7.2/bin/julia /afs/ifh.de/user/a/angeltak/Schwinger-Wilson-MPS/run_Schwinger_Wilson.jl $N $x $mg $D
 done
