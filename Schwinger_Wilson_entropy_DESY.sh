4d1a1a912e63ecf87de69fead88c2113a512b91d
TakisAngelides <takis.angelides@gmail.com>
2022-03-17 12:57:01 +0100

    debug

--- Schwinger_Wilson_entropy_DESY.sh
+++ Schwinger_Wilson_entropy_DESY.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-export JULIA_DEPOT_PATH="/lustre/fs23/group/nic/skuehn/.julia:$JULIA_DEPOT_PATH"
+export JULIA_DEPOT_PATH="/lustre/fs23/group/nic/tangelides/.julia:$JULIA_DEPOT_PATH"
 echo $JULIA_DEPOT_PATH
 N=$1
 x=$2
