116b1b5827afd5f974bc9561229f2030a39530d0
TakisAngelides <takis.angelides@gmail.com>
2022-03-30 14:17:54 +0200

    choice 3 for computing the average E field

--- run_Schwinger_Wilson.jl
+++ run_Schwinger_Wilson.jl
@@ -46,6 +46,8 @@ if choice == 1
     generate_entropy_data(mg, x, N, D, accuracy, lambda, l_0, max_sweep_number)
 elseif choice == 2
     mps_to_entropy_save_file(mg, x, 2*N, D)
+elseif choice == 3
+    mps_to_average_electric_field(mg, x, 2*N, D, l_0)
 end
 
 # ----------------------------------------------------------------------------------------------------------------------------------
