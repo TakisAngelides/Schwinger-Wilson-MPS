cd5a315633f9161eae85b52f6001dead78fabc4b
TakisAngelides <takis.angelides@gmail.com>
2022-04-24 17:34:21 +0200

    hold script will run when a job is put on hold

--- run_Schwinger_Wilson_from_dead.jl
+++ run_Schwinger_Wilson_from_dead.jl
@@ -8,16 +8,16 @@ using HDF5
 include("MPO.jl")
 include("variational_first_excited_state_MPS_algorithm.jl")
 
-accuracy = 10^(-8)
-lambda = 100.0
-l_0 = 0.0
-max_sweep_number = 100
 N = parse(Int, ARGS[1])
 x = parse(Float64, ARGS[2])
 mg = parse(Float64, ARGS[3])
 D = parse(Int64, ARGS[4])
+l_0 = parse(Float64, ARGS[5])
+lambd = parse(Float64, ARGS[6])
+acc = parse(Float64, ARGS[7])
+ms = parse(Int64, ARGS[8])
 
-mpo = get_Schwinger_Wilson_MPO(N, l_0, x, lambda, mg)
+mpo = get_Schwinger_Wilson_MPO(N, l_0, x, lambd, mg)
 
 spin_N = 2*N
 
@@ -25,7 +25,7 @@ path_to_mps = "/lustre/fs23/group/nic/tangelides/Schwinger Wilson MPS Data/N_$(s
 
 f = h5open(path_to_mps, "r")
 
-mps_group = f["$(lambda)_$(l_0)_$(mg)_$(x)_$(spin_N)_$(D)"]
+mps_group = f["$(lambd)_$(l_0)_$(mg)_$(x)_$(spin_N)_$(D)"]
 
 mps_ansatz = Vector{Array{ComplexF64}}(undef, spin_N)
 
@@ -35,4 +35,4 @@ for i in 1:spin_N
 
 end
 
-_, _, _ = variational_ground_state_MPS_from_dead_for_saving(2*N, 2, D, mpo, accuracy, max_sweep_number, mps_ansatz)
+_, _, _ = variational_ground_state_MPS_from_dead_for_saving(2*N, 2, D, mpo, acc, ms, mps_ansatz)
